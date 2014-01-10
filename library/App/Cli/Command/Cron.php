486186fe8798856e840465edbf73a853f3dcc5fc
Harmen Janssen <harmen@grrr.nl>
2014-01-10 21:46:01 +0100

    Better default cronjobs for cleaning old cluster job records.

--- library/App/Cli/Command/Cron.php
+++ library/App/Cli/Command/Cron.php
@@ -3,7 +3,10 @@
 class App_Cli_Command_Cron extends Garp_Cli_Command {
 	
 	public function frequently() {
-
+		$config = Zend_Registry::get('config');
+		if ($config->app->clusteredHosting) {
+			$this->_runClusterJobs();
+		}
 	}
 
 	public function hourly() {
@@ -11,6 +14,22 @@ class App_Cli_Command_Cron extends Garp_Cli_Command {
 	}
 
 	public function daily() {
-
+		$config = Zend_Registry::get('config');
+		if ($config->app->clusteredHosting) {
+			$this->_cleanClusterJobs();
+		}
 	}
+
+	/** Run cluster jobs */
+	protected function _runClusterJobs() {
+		$cluster_cmd = new Garp_Cli_Command_Cluster();
+		$cluster_cmd->main(array('run'));
+	}
+
+	/** Clean cluster jobs */
+	protected function _cleanClusterJobs() {
+		$cluster_cmd = new Garp_Cli_Command_Cluster();
+		$cluster_cmd->main(array('clean'));
+	}
+
 }
--- public/js/build/dev/main.js
+++ public/js/build/dev/main.js
@@ -1,2 +1,5 @@
 document.write("<script src=\"/js/src/libs/fastclick.js\"></script>");
+document.write("<script src=\"/js/garp/front/styling.js\"></script>");
+document.write("<script src=\"/js/garp/front/flashmessage.js\"></script>");
+document.write("<script src=\"/js/garp/front/cookies.js\"></script>");
 document.write("<script src=\"/js/src/main.js\"></script>");
