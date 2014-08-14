f93b36c5f8cd1ba5d1549ce494fb715dbd521795
Harmen Janssen <harmen@grrr.nl>
2014-08-14 12:13:02 +0200

    Adding semver to build paths

--- public/js/build/dev/main.js
+++
@@ -1,6 +0,0 @@
-document.write("<script src=\"/js/src/libs.js\"></script>");
-document.write("<script src=\"/js/garp/front/styling.js\"></script>");
-document.write("<script src=\"/js/garp/front/flashmessage.js\"></script>");
-document.write("<script src=\"/js/garp/front/cookies.js\"></script>");
-document.write("<script src=\"/js/src/main.js\"></script>");
-document.write("<script src=\"http://127.0.0.1:35728/livereload.js?snipver=1\"></script>");
