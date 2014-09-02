c6ba43133376d7a5f16f098558e76cde4003eca6
David Spreekmeester <david@grrr.nl>
2014-09-02 12:57:31 +0200

    New Capistrano 3 config files.

--- application/configs/deploy.rb
+++ application/configs/deploy.rb
@@ -1,24 +1,10 @@
-# set :application, "MY_CUTE_APP_NAME"
-# set :repository,  "git@code.grrr.nl:grrr/MY_LOVELY_REPOSITORY"
+set :application, '????????????????'
+set :repo_url, 'git@code.grrr.nl:grrr/???????????????'
 
-
-# task :production do
-#   server "MY_SEXY_SSH_HOST", :app, :web, :db, :primary => true
-#   set :deploy_to, "/EXAMPLE/EXAMPLE/public"
-#   set :user, "MY_BEDAZZLING_SSH_USERNAME"
-#   set :garp_env, "production"
-# end
-
-# task :staging do
-#   server "MY_SEXY_SSH_HOST", :app, :web, :db, :primary => true
-#   set :deploy_to, "/EXAMPLE/EXAMPLE/web"
-#   set :user, "MY_BEDAZZLING_SSH_USERNAME"
-#   set :garp_env, "staging"
-# end
-
-# task :integration do
-#   server "MY_CUTE_APP_NAME.integration.grrr.nl", :app, :web, :db, :primary => true
-#   set :deploy_to, "/var/www/MY_CUTE_APP_NAME.integration.grrr.nl"
-#   set :user, "garp-deployer"  
-#   set :garp_env, "integration"
-# end
+namespace :deploy do
+	task :started do
+		on roles(:web) do
+			info "No app-specific deploy tasks in this project."
+		end
+	end
+end
