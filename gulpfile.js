28f89827f3fb11b0b1c0a5927faeb046702a2fc3
Mattijs <mattijs@grrr.nl>
2014-10-16 12:12:33 +0200

    Removed unused files

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,38 @@
 /*
- * Gulp is used to manage and generate front-end assets, such as
- * css, javascript, and compressed images. You can run gulp from
- * the command line with the commands below.
+ * Gulpfile
+ * Author: Mattijs Bliek
  *
  * IMPORTANT:
  * Gulp needs to be installed globally on your system before
  * you're able to run tasks. This can be done by running:
  * `npm install -g gulp`
  *
- * OWNER:
- * Mattijs Bliek
+ * -------------------------------------------------------------
+ *
+ * DESCRIPTION:
+ * Gulp is used to manage and generate front-end assets, such as
+ * css, javascript, and compressed images. You can run gulp from
+ * the command line with the commands below.
+ *
+ * -------------------------------------------------------------
+ *
+ * TODO:
+ * - Integrate something to generate icons (probably gulp-svg-sprites)
+ * - CSS sourcemaps are not rendering properly
+ * - Implement gulp-css-url-adjuster to generate cdn img paths
+ * - Either concat libs.js and main.js for production, or employ a module
+ *   loader to generate various builds
+ *
+ * -------------------------------------------------------------
  *
  * COMMANDS:
- * Generate a build for an environment (set environment with --e):
+ *
+ * Generate a build for an environment:
  * `gulp`
+ *
+ * Arguments:
+ * --e=environment (development|staging|production)
+ * --skipImages (don't build compressed images)
  *
  * Generate images for an environment:
  * `gulp images`
@@ -21,6 +40,7 @@
  * Watch files and run a server (use for development only):
  * `gulp watch`
  *
+ * -------------------------------------------------------------
  */
 var gulp           = require('gulp'),
 	autoprefixer   = require('gulp-autoprefixer'),
@@ -32,7 +52,6 @@ var gulp           = require('gulp'),
 	modernizr      = require('gulp-modernizr'),
 	sass           = require('gulp-sass'),
 	browserSync    = require('browser-sync'),
-	ini            = require('multi-ini'),
 	merge          = require('merge'),
 	jshintStylish  = require('jshint-stylish'),
 	mainBowerFiles = require('main-bower-files'),
@@ -45,49 +64,43 @@ var gulp           = require('gulp'),
 	argv           = require('yargs').argv,
 	sh             = require('execSync');
 
-var config,
-	semver;
+var semver;
 var paths = {};
 var ENV = argv.e ? argv.e : 'development';
-
-function getConfig() {
-	var zendApp = ini.read('./application/configs/app.ini');
-	var zendAssets = ini.read('./application/configs/assets.ini');
-
-	var zendConfig = {};
-	for (var i in zendApp) {
-		var shortname = i.split(" ")[0];
-		zendConfig[shortname] = merge(zendApp[i],zendAssets[i]);
-	}
-	return zendConfig;
-};
 
 function getShellOutput(command) {
 	result = sh.exec(command);
     if (result.stderr) {
-        gutil.log(gutil.colors.red('Error getting shell output: ' + result.stderr));
+		handleError('Error getting shell output: ' + result.stderr);
         gutil.beep();
     }
     // Do a replace because of newline in shell output
     return result.stdout.replace(/\s?$/, '');
 };
 
+function getConfigValue(value) {
+	if (!value) {
+		handleError('Can\'t get undefined config value');
+		return;
+	}
+	var command = 'php ' + __dirname + '/../golem/scripts/golem.php config get ' + value + ' --e=' + ENV;
+	return getShellOutput(command);
+}
+
 function constructPaths() {
 	paths.public      = 'public';
 
 	paths.js          = paths.public + '/js';
 	paths.jsSrc       = paths.js     + '/src';
-	paths.jsBuild     = paths.public + '/' + config[ENV].assets.js.root + '/' + semver;
+	paths.jsBuild     = paths.public + '/' + getConfigValue('assets.js.root') + '/' + semver;
 
 	paths.css         = paths.public + '/css';
-	paths.cssSrc      = paths.public + '/' + config.production.assets.sass.root;
-	paths.cssBuild    = paths.public + '/' + config[ENV].assets.css.root + '/' + semver;
+	paths.cssSrc      = paths.public + '/' + getConfigValue('assets.sass.root');
+	paths.cssBuild    = paths.public + '/' + getConfigValue('assets.css.root') + '/' + semver;
 
 	paths.imgSrc      = paths.css      + '/img';
 	paths.imgBuild    = paths.cssBuild + '/img';
-	paths.icons       = paths.css      + 'icons/';
-	paths.fonts       = paths.css      + 'build/fonts/icons/';
-	//paths.cdn         = config[ENV].assets.cdn.domain;
+	paths.cdn         = getConfigValue('assets.cdn.domain');
 	return paths;
 };
 
@@ -97,27 +110,22 @@ function handleError (error) {
 }
 
 gulp.task('init', function() {
-	config = getConfig();
 	semver = getShellOutput('semver');
+	domain = getConfigValue('app.domain');
 	paths  = constructPaths();
 	gutil.log(gutil.colors.green('Semver: ' + semver));
 	gutil.log(gutil.colors.green('Environment: ' + ENV));
 });
 
 gulp.task('browser-sync', ['sass-ie', 'sass-cms', 'sass', 'javascript'], function() {
-	if(!config.development.app.domain) {
-		gutil.log(gutil.colors.red('No domain set in application/configs/app.ini'));
+	if(!domain) {
+		handleError('Could not get ' + ENV + ' domain from application/configs/app.ini');
 	}
 	browserSync({
-		proxy: config.development.app.domain
+		proxy: domain
 	});
 });
 
-/**
- * TODO:
- * - Sourcemaps are not rendering properly
- * - Implement gulp-css-url-adjuster to generate cdn img paths
- */
 gulp.task('sass', ['scss-lint'], function () {
     gutil.log(gutil.colors.green('Building css to ' + paths.cssBuild));
     return gulp.src([paths.cssSrc + '/base.scss'])
@@ -154,17 +162,12 @@ gulp.task('scss-lint', ['init'], function() {
 		.pipe(scsslint({'config': __dirname + '/.scss-lint.yml'})).on('error', handleError);
 });
 
-/*
- * TODO:
- * Either concat libs.js and main.js for production, or employ a module
- * loader to generate various builds (e.g. document ready, on load etc.)
- */
 gulp.task('javascript', ['jshint', 'bower-concat'], function() {
     gutil.log(gutil.colors.green('Building js to ' + paths.jsBuild));
     return gulp.src(paths.jsSrc + '/**/*.js')
 	.pipe(gulpif(ENV == 'development', sourcemaps.init()))
 		.pipe(concat('main.js'))
-		.pipe(gulpif(ENV == 'development', uglify(), uglify({ 'compress': { 'pure_funcs': ['console.log'] } })))
+		.pipe(gulpif(ENV == 'development', uglify(), uglify({ 'compress': { 'pure_funcs': ['console.log'] } }))).on('error', handleError)
 	.pipe(gulpif(ENV == 'development', sourcemaps.write()))
     .pipe(gulp.dest(paths.jsBuild));
 });
@@ -181,15 +184,15 @@ gulp.task('bower-concat', function() {
 });
 
 gulp.task('jshint', function () {
+    gutil.log(paths.jsSrc);
     gulp.src(paths.jsSrc + '/**/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 /**
- * NOTE: this task produces erros which are safe to ignore,
- * this is due to Modernizr not being tool agnostic and
- * looking for grunt modules.
+ * NOTE: this task produces erros which are safe to ignore, this is due
+ * to Modernizr not being tool agnostic and looking for grunt modules.
  */
 gulp.task('modernizr', ['sass', 'javascript'], function() {
   return gulp.src([paths.cssBuild + '/base.css', paths.jsBuild + '/main.js'])
@@ -199,12 +202,18 @@ gulp.task('modernizr', ['sass', 'javascript'], function() {
 });
 
 gulp.task('tinypng', function () {
+	if (argv.skipImages) {
+		return;
+	}
     return gulp.src(paths.imgSrc + '/*.png')
         .pipe(tinypng('GpST4zD3uNuHeQbIw7zs3hiL-raKJGMF'))
         .pipe(gulp.dest(paths.imgBuild));
 });
 
 gulp.task('images', ['init', 'tinypng'], function () {
+	if (argv.skipImages) {
+		return;
+	}
     return gulp.src(paths.imgSrc + '/*.{gif,jpg,svg}')
         .pipe(imagemin({
             progressive: true,
