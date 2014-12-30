69a0513c80feb7abdf48cea7312d0aec4b4a4d15
Harmen Janssen <harmen@grrr.nl>
2014-12-30 17:46:59 +0100

    Removed git merge artifacts. Tsk tsk tsk.

--- garp/scripts/pull_changes.sh
+++ garp/scripts/pull_changes.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 if [ -d "garp" ]; then
-<<<<<<< HEAD
-	git subtree pull -P garp --squash git@code.grrr.nl:grrr/garp3 master
-=======
 	git subtree pull -P garp --squash git@github.com:grrr-amsterdam/garp3 master
->>>>>>> 2003f3421883bf4e997378d8d830e797926e2f94
 fi
--- garp/scripts/push_changes.sh
+++ garp/scripts/push_changes.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 if [ -d "garp" ]; then
-<<<<<<< HEAD
-	git subtree push -P garp git@code.grrr.nl:grrr/garp3 master
-=======
 	git subtree push -P garp git@github.com:grrr-amsterdam/garp3 master
->>>>>>> 2003f3421883bf4e997378d8d830e797926e2f94
 fi
