57fb31c82a3985f4508fe5619d4e257283548ed4
David Spreekmeester <david@grrr.nl>
2015-02-24 15:13:34 +0100

    Squashed 'garp/' changes from 3d85a02..138ae7b
    138ae7b Removed extra parameter from assetUrl, since we include Modernizr via baseUrl now;
    62e31be Added extra comment;
    801256b Styled the wrong cookie view in another project, got pushed back to Garp. Undo and gpg from here;
    8630362 Made sure we can also choose not to append semver in assetUrl; made sure we still get the right extenstion after we've appended Semver;
    746a30c Removed semver from gulpfile; added default sort order for basic models + translation for AuthPasswordless;
    d747480 Made sure all assets are using the appended Garp_Semver;
    5f7c4e2 Updated assetUrl with versioned query strings;
    
    git-subtree-dir: garp
    git-subtree-split: 138ae7bca99acb3d88c8e66c8ee216b963c62c74

--- application/modules/g/views/helpers/AssetUrl.php
+++ application/modules/g/views/helpers/AssetUrl.php
@@ -15,6 +15,7 @@ class G_View_Helper_AssetUrl extends Zend_View_Helper_BaseUrl {
 	/**
 	 * Create a versioned URL to a file
 	 * @param String $file The file path
+	 * @param String $forced_extension Force to use an extension, even when extension doesn't match or is missing (eg. '/' in 'ASSET_URL' when using a cdn, but 'js' is 'local')
 	 * @return String
 	 */
 	public function assetUrl($file = null, $forced_extension = false) {
@@ -29,6 +30,12 @@ class G_View_Helper_AssetUrl extends Zend_View_Helper_BaseUrl {
 		// - add the current semver to the path
 		if (strpos($file, '/') === false) {
 			$file = $this->getVersionedBuildPath($file);
+
+		// Else we will use the old (but actually more "modern") approach.
+		// AssetUrl will:
+		// - append semver as query string (main.js?v0.0.1)
+		} else if (!empty($file) && substr($file, -1) !== '/') {
+			$file = $this->getVersionedQuery($file);
 		}
 
 		// For backwards compatibility: deprecated param assetType
@@ -48,6 +55,9 @@ class G_View_Helper_AssetUrl extends Zend_View_Helper_BaseUrl {
 		if (!$file) {
 			return;
 		}
+
+		// strip appended semver
+		$file = substr($file, 0, strpos($file, '?v'));
 
 		$fileParts = explode('.', $file);
 		$lastPart = $fileParts[sizeof($fileParts) - 1];
@@ -96,6 +106,10 @@ class G_View_Helper_AssetUrl extends Zend_View_Helper_BaseUrl {
 		}
 
 		return rtrim($baseUrl, '/') . '/' . $file;
+	}
+
+	public function getVersionedQuery($file) {
+		return $file . '?' . new Garp_Semver();
 	}
 
 	public function getVersionedBuildPath($file) {
