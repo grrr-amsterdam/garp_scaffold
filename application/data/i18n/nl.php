5816970b7854faad117661e460e3495a75916c96
Harmen Janssen <harmen@grrr.nl>
2016-03-02 14:20:42 +0100

    Changed deprecated path to Garp application

--- application/data/i18n/nl.php
+++ application/data/i18n/nl.php
@@ -3,7 +3,7 @@
  * Provide translation strings.
  * The Garp translation file provides a variable called $translationTable. Append terms here if necessary.
  */
-include APPLICATION_PATH.'/../garp/application/data/i18n/nl.php';
+include GARP_APPLICATION_PATH . '/data/i18n/nl.php';
 return array_merge($translationTable, array(
 
 ));
