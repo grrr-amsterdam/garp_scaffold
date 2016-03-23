34023d000b3ddd1d92b6d48659876b78a871b873
Harmen Janssen <harmen@grrr.nl>
2016-03-23 08:11:03 +0100

    Added defaults for cdn and ENV
    Now uses environment.php to determine APPLICATION_ENV and set cdn.ssl to
    true by default.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,15 @@
 <?php
 date_default_timezone_set('Europe/Amsterdam');
-define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-require_once("../garp/application/init.php");
 
+require_once('../application/configs/environment.php');
+
+define('BASE_PATH', realpath(dirname(__FILE__) . '/..'));
+require_once("../vendor/grrr-amsterdam/garp3/application/init.php");
+require_once('../vendor/autoload.php');
 
 // Create application, bootstrap, and run
 $application = new Garp_Application(
-	APPLICATION_ENV, 
+	APPLICATION_ENV,
 	APPLICATION_PATH.'/configs/application.ini'
 );
 Zend_Registry::set('application', $application);
