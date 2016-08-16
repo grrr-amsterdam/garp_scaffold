8b366972a20c75b7e94e4582bf88f61ac1e2dfbd
Harmen Janssen <harmen@grrr.nl>
2016-08-16 09:31:36 +0200

    Composer update

--- application/data/i18n/nl.php
+++ application/data/i18n/nl.php
@@ -1,9 +1,18 @@
 <?php
 /**
  * Provide translation strings.
- * The Garp translation file provides a variable called $translationTable. Append terms here if necessary.
+ * The Garp translation file provides a variable called $translationTable.
+ * Append terms here if necessary.
+ *
+ * @package Garp
+ * @author  Harmen Janssen <harmen@grrr.nl>
  */
-include GARP_APPLICATION_PATH . '/data/i18n/nl.php';
-return array_merge($translationTable, array(
-
-));
+require GARP_APPLICATION_PATH . '/data/i18n/nl.php';
+return array_merge(
+    $translationTable,
+    array(
+        // @codingStandardsIgnoreStart
+        'Dog' => 'Hond',
+        // @codingStandardsIgnoreEnd
+    )
+);
--- vendor/composer/include_paths.php
+++ vendor/composer/include_paths.php
@@ -7,10 +7,10 @@ $baseDir = dirname($vendorDir);
 
 return array(
     $vendorDir . '/zendframework/zendframework1/library',
-    $vendorDir . '/grrr-amsterdam/garp3/library',
     $vendorDir . '/phpunit/phpunit-mock-objects',
     $vendorDir . '/phpunit/php-token-stream',
     $vendorDir . '/phpunit/php-code-coverage',
     $vendorDir . '/phpunit/phpunit',
     $vendorDir . '/symfony/yaml',
+    $vendorDir . '/grrr-amsterdam/garp3/library',
 );
