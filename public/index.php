b2c192dd4a951077443234707cf9bc202ec7e996
Harmen Janssen <harmen@grrr.nl>
2016-08-19 16:08:16 +0200

    Added Sentry stub

--- public/index.php
+++ public/index.php
@@ -1,17 +1,24 @@
 <?php
 date_default_timezone_set('Europe/Amsterdam');
-
-require_once('../application/configs/environment.php');
-
+require_once '../application/configs/environment.php';
 define('BASE_PATH', realpath(dirname(__FILE__) . '/..'));
 
-require_once('../vendor/autoload.php');
-require_once("../vendor/grrr-amsterdam/garp3/application/init.php");
+/*
+define(
+    'SENTRY_API_URL',
+    // @codingStandardsIgnoreStart
+    'your Sentry url here'
+    // @codingStandardsIgnoreEnd
+);
+*/
+
+require_once '../vendor/autoload.php';
+require_once "../vendor/grrr-amsterdam/garp3/application/init.php";
 
 // Create application, bootstrap, and run
 $application = new Garp_Application(
-	APPLICATION_ENV,
-	APPLICATION_PATH.'/configs/application.ini'
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
 );
 Zend_Registry::set('application', $application);
 
